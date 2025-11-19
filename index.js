edcf2ffcb3a6976af64aa76ade18fb0feacab1ea
unknown <deeppatel200590>
2025-11-19 18:49:41 +0530

    fixed port issue

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ require ('dotenv').config()
 const express = require('express')//below line same as this line
 // import express from 'express'
 const app = express()
-const port = 3000
+const port = 10000
 
 // localhost:3000 run this on the webpage and see result also we can run 'num run start' in terminal
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-//localhost:3000/hi run this and see result
+//localhost:10000/hi run this and see result
 app.get('/hi', (req, res) => {
   res.send('Hi!')
 })
@@ -19,7 +19,7 @@ app.get('/login', (req, res) => {
   res.send('Hi from deep!')
 })
 
-//this run 3000 because js overwrite the .env file
+//this run 10000 because js overwrite the .env file
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${process.env.PORT || port}`)
 })
